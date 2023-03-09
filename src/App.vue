30888de9f27a52b64c6aa4065030b6cfafe95ff8
jswdl <1136751548@qq.com>
2023-03-09 21:20:56 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+	  <el-row>
+	    <el-col :span="24"><div class="grid-content bg-purple-dark"></div></el-col>
+	  </el-row>
+	  <el-row>
+	    <el-col :span="12"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
+	  </el-row>
+	  <el-row>
+	    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
+	    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+	  </el-row>
+	  <el-row>
+	    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+	    <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
+	  </el-row>
+	  <el-row>
+	    <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+	    <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+	    <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
+	    <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+	  </el-row>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
